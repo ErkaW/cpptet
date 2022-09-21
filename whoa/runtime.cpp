2b954c1920db670cfe93cca646194b526b0b11b6
Mohammad Amrizal Kurniansyah <65645882+Erka11@users.noreply.github.com>
2022-09-21 20:20:12 +0700

    coba wtf

---
+++ whoa/runtime.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "wtf1.cpp"
+#include "wtf2.cpp"
+int main(){
+    name();
+}
---
+++ whoa/wtf1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void name(){
+    string name1;
+    cout << "Masukkan nama:"; cin >> name1;
+    cout << name1 << endl;
+}
