534b0984f20130b4a0604447cdddca933912fc45
Mohammad Amrizal Kurniansyah <65645882+ErkaW@users.noreply.github.com>
2022-10-13 14:36:58 +0700

    gitignore for output folder + new tugas

---
+++ tugasinputoutput.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i,n;
+    float a,b;
+    cout << "Program Penjumlahan Bilangan" << endl;
+    cout << endl;
+    cout << "Masukkan jumlah angka yang akan ditambah : ";
+    cin >> n;
+    cout << endl;
+    b=0,i=1;
+    Putar:
+    if(i<=n){
+        cout << "Masukkan angka ke-" << i << " : ";
+        cin >> a;
+        b = b + a;
+        cout << "Hasil penjumlahan : " << b << endl << endl;
+        i++;
+        goto Putar;
+    }else{
+        cout << "Hasil akhir penjumlahan : " << b << endl;
+    }
+    return 0;
+}
+
